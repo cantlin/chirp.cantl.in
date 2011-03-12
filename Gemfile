bccc3dfb141031f42a432a9cfca84b917b11f510
Cantlin Ashrowan <cantlin@ashrowan.com>
2011-03-12 15:19:31 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+
+gem 'mysql2'
+gem 'oauth-plugin', '>=0.4.0.pre1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chirp::Application.config.secret_token = '64b82bb04ab7ab427def3920e2966dc6b8b7914aaaf42d6e49637ce3bf308ace6149a4a48e0a55d0df9c0adf733f34e57807156820b2cb7943c0e492c1d1addb'
